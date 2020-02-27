53d6ca366eea897cd346457803bed0ef7db5ea65
bichous <bichous@users.noreply.github.com>
2020-02-27 02:25:19 -0600

    deploy minor fixes XD

--- frontend/src/services/product.js
+++ frontend/src/services/product.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 let baseURL;
 
 process.env.NODE_ENV === 'production'
-  ? (baseURL = 'here should be your production endpoint')
+  ? (baseURL = 'https://laboratoria-pt.herokuapp.com')
   : (baseURL = 'http://localhost:3000');
 
 const MY_SERVICE = axios.create({ withCredentials: true, baseURL });
