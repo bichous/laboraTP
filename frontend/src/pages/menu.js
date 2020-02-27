0a217e28fe83de133fe6b1acd54a0bf7bf94684e
bichous <bichous@users.noreply.github.com>
2020-02-27 02:09:49 -0600

    state done, missing service to sen the order

--- frontend/src/pages/menu.js
+++ frontend/src/pages/menu.js
@@ -15,10 +15,13 @@ import PRODUCT_SERVICE from '../services/product';
 function Menu() {
   const [inicialProducts, setProducts] = useState([]);
   const [inicialOrder, setOrder] = useState([]);
-  const [cliked, setCliked] = useState(false);
+  const [noOrder, setNoOrder] = useState(true);
 
   let buttonsMenu;
+  let productsOrder;
   let productList = [];
+  let newOrder = [];
+  let totalPrice = 0;
 
   const getAllProducts = () => {
     return PRODUCT_SERVICE.allProducts();
@@ -38,11 +41,9 @@ function Menu() {
       element => element.menu === 'Breakfast'
     );
     setProducts(breakfastProducts);
-    console.log(inicialProducts);
   };
 
   const settingOrder = (arr1, arr2) => {
-    let newOrder = [];
     arr1.map(product => {
       if (arr2.includes(product._id)) {
         newOrder.push(product);
@@ -51,10 +52,6 @@ function Menu() {
     setOrder(newOrder);
   };
 
-  // useEffect(() => {
-  //   settingOrder(inicialProducts, productList);
-  // }, []);
-
   const hanldeClick = e => {
     const {
       target: { value, id }
@@ -62,11 +59,9 @@ function Menu() {
 
     if (!productList.includes(value)) {
       productList.push(value);
-      console.log(productList);
     } else {
       const index = productList.indexOf(value);
       productList.splice(index, 1);
-      console.log(productList);
     }
   };
 
@@ -99,6 +94,26 @@ function Menu() {
     ));
   }
 
+  if (inicialOrder.length !== 0 && inicialOrder.length == 1) {
+    productsOrder = inicialOrder.map((e, i) => (
+      <li key={i}>
+        {e.name}....... ${e.price}
+      </li>
+    ));
+    totalPrice = inicialOrder[0].price;
+  }
+
+  if (inicialOrder.length !== 0 && inicialOrder.length > 1) {
+    productsOrder = inicialOrder.map((e, i) => (
+      <li key={i}>
+        {e.name}....... ${e.price}
+      </li>
+    ));
+    for (let i = 0; i < inicialOrder.length; i++) {
+      totalPrice += inicialOrder[i].price;
+    }
+  }
+
   return (
     <MyContext.Consumer>
       {context => (
@@ -127,15 +142,25 @@ function Menu() {
             >
               {buttonsMenu}
             </Flex>
-            <Flex w="35vw" h="80vh" justify="center" alignItems="center">
-              <h1>Aqui va la orden</h1>
-              <Button
-                onClick={() => {
-                  settingOrder(inicialProducts, productList);
-                }}
-              >
-                Order
-              </Button>
+            <Flex
+              w="35vw"
+              h="80vh"
+              justify="center"
+              alignItems="center"
+              direction={'column'}
+            >
+              <ul>{productsOrder}</ul>
+              Total price = ${totalPrice}
+              <Flex justify="space-between" w="20vw">
+                <Button
+                  onClick={() => {
+                    settingOrder(inicialProducts, productList);
+                  }}
+                >
+                  Preview
+                </Button>
+                <Button>Order</Button>
+              </Flex>
             </Flex>
           </Flex>
         </>
